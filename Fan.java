132a35185a7cf3ea07af197daad08f3a956f401b
priyamnaik9066-eng <priyamnaik9066@gmail.com>
2026-07-13 13:07:45 +0530

    java code

---
+++ Fan.java
@@ -0,0 +1,81 @@
+class Fan { 
+    static boolean isConnected; 
+    static int maxSpeed = 7; 
+    static int currentSpeed; 
+    static int minSpeed; 
+
+    public static void onOroff(){ 
+        if (isConnected == false){ 
+            isConnected = true; 
+            System.out.println("Fan is connected..."); 
+        } else if (isConnected == true){ 
+            isConnected = false; 
+            System.out.println("Fan is dis-connected..."); 
+        } 
+        return; 
+    } 
+
+    public static void increaseSpeed(){ 
+        if(isConnected == true){ 
+            if(currentSpeed < maxSpeed){ 
+                currentSpeed = currentSpeed + 1; 
+                System.out.println("The currentSpeed is " + currentSpeed); 
+            } else if (currentSpeed == maxSpeed) { 
+                System.out.println("Max Speed reached..."); 
+            } 
+        } else if (isConnected == false) { 
+            System.out.println("Turn On the Fan first..."); 
+        } 
+    } 
+
+    public static void decreaseSpeed(){ 
+        if(isConnected == true){ 
+            if(currentSpeed > minSpeed){ 
+                currentSpeed = currentSpeed - 1; 
+                System.out.println("The currentSpeed is " + currentSpeed); 
+            } else if (currentSpeed == minSpeed) { 
+                System.out.println("Max Speed reached..."); 
+            } 
+        } else if (isConnected == false) { 
+            System.out.println("Turn On the Fan first..."); 
+        } 
+    } 
+
+    public static void main(String[] args){ 
+        
+        System.out.println("main started"); 
+        System.out.println(isConnected); 
+        onOroff(); 
+        System.out.println(isConnected); 
+        onOroff(); 
+        System.out.println("main ended"); 
+
+        
+        System.out.println("main started"); 
+        System.out.println(isConnected); 
+        onOroff(); 
+        increaseSpeed(); 
+        increaseSpeed(); 
+        increaseSpeed(); 
+        increaseSpeed(); 
+        increaseSpeed(); 
+        increaseSpeed(); 
+        increaseSpeed(); 
+        onOroff(); 
+        System.out.println("main ended"); 
+
+        
+        System.out.println("main started"); 
+        System.out.println(isConnected); 
+        onOroff(); 
+        decreaseSpeed(); 
+        decreaseSpeed(); 
+        decreaseSpeed(); 
+        decreaseSpeed(); 
+        increaseSpeed(); 
+        decreaseSpeed(); 
+        decreaseSpeed(); 
+        onOroff(); 
+        System.out.println("main ended"); 
+    } 
+}
