b55e4f1577fd2284b0d960f0e1cc3aa243fa2f35
priyamnaik9066-eng <priyamnaik9066@gmail.com>
2026-07-14 10:23:29 +0530

    java code

---
+++ Dmart.java
@@ -0,0 +1,81 @@
+class Dmart {
+    
+	  public static void main(String [] arg)
+	  {
+	  
+	         String groceries [] = { "Atta","Dals","Rice","Oil","Dry Fruits","Masalas"};
+			 String clothes []  =  { "H&M","Zara","Levi's","Vero Moda","Libas","Fabindia"};
+			 String shoes  [] = {"Nike","Adidas","Walkaroo","Relaxo","Puma","Bata"};
+			 String cosmetics [] = {"LipStick","Foundation","Concealer","Blush","Moisturizer","Sunscreen"};
+			 String toys [] = {"Teddy Bear","Play Kitchen","Soft Ball","Puzzle Set","Spinning Top","Toy Train"};
+			 String perfumes [] = {"Gucci","Chanel","Dior","Versace","Aerin","Chloe"};
+			 
+			 
+			 
+			 
+			 
+			 System.out.println(groceries[0]);
+             System.out.println(groceries[1]);
+             System.out.println(groceries[2]);
+             System.out.println(groceries[3]);
+             System.out.println(groceries[4]);
+             System.out.println(groceries[5]);
+             
+			 
+			 
+			 
+             
+			 System.out.println(clothes[0]);
+             System.out.println(clothes[1]);
+             System.out.println(clothes[2]);
+             System.out.println(clothes[3]);
+             System.out.println(clothes[4]);
+             System.out.println(clothes[5]);
+             
+			 
+			 
+			 
+			 System.out.println(shoes[0]);
+             System.out.println(shoes[1]);
+             System.out.println(shoes[2]);
+             System.out.println(shoes[3]);
+             System.out.println(shoes[4]);
+             System.out.println(shoes[5]);
+             
+             
+			 
+			 
+			 
+			 System.out.println(cosmetics[0]);
+             System.out.println(cosmetics[1]);
+             System.out.println(cosmetics[2]);
+             System.out.println(cosmetics[3]);
+             System.out.println(cosmetics[4]);
+             System.out.println(cosmetics[5]);
+             
+             
+			 
+			 
+			 System.out.println(toys[0]);
+             System.out.println(toys[1]);
+             System.out.println(toys[2]);
+             System.out.println(toys[3]);
+             System.out.println(toys[4]);
+             System.out.println(toys[5]);
+             
+             
+			 
+			 
+			 
+			 System.out.println(perfumes[0]);
+             System.out.println(perfumes[1]);
+             System.out.println(perfumes[2]);
+             System.out.println(perfumes[3]);
+             System.out.println(perfumes[4]);
+             System.out.println(perfumes[5]);
+             
+       }
+
+}	   
+             
+			 
