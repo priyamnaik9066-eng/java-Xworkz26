eb275c498ff7b92f14fc71bf2635da9cb21c27b4
priyamnaik9066-eng <priyamnaik9066@gmail.com>
2026-07-14 10:28:14 +0530

    java code

---
+++ OTTPlatform.java
@@ -0,0 +1,49 @@
+class OTTPlatform {
+
+   public static void main(String []arg){
+
+      String netflix[] = {
+         "Kannada","English","Hindi","Tamil","Telugu",
+         "Malayalam","Marathi","Bengali","Punjabi","Gujarati",
+         "Spanish","French","German","Japanese","Korean",
+         "Chinese","Italian","Russian","Arabic","Turkish",
+         "Portuguese","Thai","Indonesian","Urdu","Vietnamese"
+      };
+
+      String amazonPrime[] = {
+         "Kannada","English","Hindi","Tamil","Telugu",
+         "Malayalam","Marathi","Bengali","Punjabi","Gujarati",
+         "Spanish","French","German","Japanese","Korean",
+         "Chinese","Italian","Russian","Arabic","Turkish",
+         "Portuguese","Thai","Indonesian","Urdu","Vietnamese"
+      };
+
+      String disneyHotstar[] = {
+         "Kannada","English","Hindi","Tamil","Telugu",
+         "Malayalam","Marathi","Bengali","Punjabi","Gujarati",
+         "Spanish","French","German","Japanese","Korean",
+         "Chinese","Italian","Russian","Arabic","Turkish",
+         "Portuguese","Thai","Indonesian","Urdu","Vietnamese"
+      };
+
+      System.out.println(netflix[0]);
+      System.out.println(netflix[1]);
+      System.out.println(netflix[2]);
+      System.out.println(netflix[3]);
+      System.out.println(netflix[4]);
+
+      System.out.println(amazonPrime[0]);
+      System.out.println(amazonPrime[1]);
+      System.out.println(amazonPrime[2]);
+      System.out.println(amazonPrime[3]);
+      System.out.println(amazonPrime[4]);
+
+      System.out.println(disneyHotstar[0]);
+      System.out.println(disneyHotstar[1]);
+      System.out.println(disneyHotstar[2]);
+      System.out.println(disneyHotstar[3]);
+      System.out.println(disneyHotstar[4]);
+
+   }
+
+}
