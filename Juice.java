bd1e115786a5e4eb161a8268b7c49c1e0d3c44d9
priyamnaik9066-eng <priyamnaik9066@gmail.com>
2026-07-14 10:24:39 +0530

    java code

---
+++ Juice.java
@@ -0,0 +1,21 @@
+class Juice {
+	
+	
+	public static void main (String [] args)
+	{
+	  
+	       String juiceNames[] ={"Mango Juice","Apple Juice","Orange Juice","Lemon Juice","Pienapple Juice","papaya Juice","Avocado Juice","Sapota Juice"};
+	       System.out.println(juiceNames[0]);
+		   System.out.println(juiceNames[1]);
+		   System.out.println(juiceNames[2]);
+		   System.out.println(juiceNames[3]);
+		   System.out.println(juiceNames[4]);
+		   System.out.println(juiceNames[5]);
+		   System.out.println(juiceNames[6]);
+		   System.out.println(juiceNames[7]);
+		   
+		   
+		   
+    }
+	       
+}	   
