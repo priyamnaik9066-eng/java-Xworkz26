8cc0065ad06a693e2099386c2b6d0a7d3f281e4b
priyamnaik9066-eng <priyamnaik9066@gmail.com>
2026-07-14 10:21:24 +0530

    java code

---
+++ TeaShop.java
@@ -0,0 +1,18 @@
+ class  TeaShop {
+ 
+
+  // starting point 
+    public static void main(String[] ref)
+	{
+	  // code
+	  //statements ;
+	  
+	      String teaNames[] = {"Masala Tea","Elaichi Tea","Lemon Tea"};
+		  System.out.println(teaNames[0]);
+          System.out.println(teaNames[1]);
+          System.out.println(teaNames[2]);		  
+	}
+
+
+
+}
