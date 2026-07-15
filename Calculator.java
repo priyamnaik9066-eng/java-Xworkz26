4e66d07b8b2e3324b01d34b6a8bdca68b34b19ec
priyamnaik9066-eng <priyamnaik9066@gmail.com>
2026-07-15 10:19:42 +0530

    java code

---
+++ Calculator.java
@@ -0,0 +1,47 @@
+class Calculator {
+   
+   
+	     
+		 
+		 public static void add(int first,int second){
+		            int total = first+second;
+				System.out.println(total);
+	}
+				
+				
+	    public static void multiply(int first,int second){
+		              int total = first*second;
+			   System.out.println(total);
+    
+	}
+			   
+		public static void divide(int first,int second){
+		             int total = first/second;
+				System.out.println(total);
+				
+	}
+				
+		public static void substract(int first ,int second){
+		              int total = first-second;
+				System.out.println(total);
+				
+	}
+				
+		public static void mod(int first ,int second){
+		              int total = first%second;
+				System.out.println(total);
+	
+	}
+				
+		
+	  public static void main(String [] cal){
+	      
+		  add (88,98);
+		  multiply(78,98);
+		  divide(98,56);
+		  substract(45,78);
+		  mod(34,67);
+		  
+	}
+		  
+}
