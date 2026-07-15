7d804ad79536ef15bc49ed14203a49f84709b16b
priyamnaik9066-eng <priyamnaik9066@gmail.com>
2026-07-15 10:10:48 +0530

    java code

---
+++ KarnatakaAirports.java
@@ -0,0 +1,47 @@
+class KarnatakaAirports {
+
+   public static void main(String[] args) {
+
+      String kempegowdaAirport = "Kempegowda International Airport, Bengaluru";
+      String mysuruAirport = "Mysuru Airport, Mysuru";
+      String mangaluruAirport = "Mangaluru International Airport, Mangaluru";
+      String hubballiAirport = "Hubballi Airport, Hubballi";
+      String belagaviAirport = "Belagavi Airport, Belagavi";
+      String kalaburagiAirport = "Kalaburagi Airport, Kalaburagi";
+      String bidarAirport = "Bidar Airport, Bidar";
+      String shivamoggaAirport = "Shivamogga Airport, Shivamogga";
+      String vijayapuraAirport = "Vijayapura Airport, Vijayapura";
+      String ballariAirport = "Jindal Vijaynagar Airport, Ballari";
+      String hassanAirport = "Hassan Airport, Hassan";
+      String koppalAirport = "Koppal Airport, Koppal";
+      String karwarAirport = "Karwar Airport, Karwar";
+      String dharwadAirport = "Dharwad Airport, Dharwad";
+      String raichurAirport = "Raichur Airport, Raichur";
+
+      String karnatakaAirports[] = {
+         kempegowdaAirport,
+         mysuruAirport,
+         mangaluruAirport,
+         hubballiAirport,
+         belagaviAirport,
+         kalaburagiAirport,
+         bidarAirport,
+         shivamoggaAirport,
+         vijayapuraAirport,
+         ballariAirport,
+         hassanAirport,
+         koppalAirport,
+         karwarAirport,
+         dharwadAirport,
+         raichurAirport
+      };
+
+      System.out.println("Karnataka Airports");
+
+      for(String airport : karnatakaAirports) {
+         System.out.println(airport);
+      }
+
+   }
+
+}
