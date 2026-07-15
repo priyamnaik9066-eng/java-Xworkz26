c05c346d4446c3bb5c1e5617b6ce38f3213f36b2
priyamnaik9066-eng <priyamnaik9066@gmail.com>
2026-07-15 09:57:29 +0530

    java code

---
+++ Agriculture.java
@@ -0,0 +1,77 @@
+class Agriculture {
+
+   public static void main(String[] args) {
+
+      String tractor = "Tractor";
+      String plough = "Plough";
+      String hoe = "Hoe";
+      String shovel = "Shovel";
+      String spade = "Spade";
+      String sickle = "Sickle";
+      String rake = "Rake";
+      String cultivator = "Cultivator";
+      String seedDrill = "Seed Drill";
+      String harvester = "Harvester";
+      String rotavator = "Rotavator";
+      String sprayer = "Sprayer";
+      String waterPump = "Water Pump";
+      String thresher = "Thresher";
+      String wheelbarrow = "Wheelbarrow";
+      String axe = "Axe";
+      String pickaxe = "Pickaxe";
+      String handWeeder = "Hand Weeder";
+      String pruningShears = "Pruning Shears";
+      String gardenFork = "Garden Fork";
+      String leveler = "Leveler";
+      String discHarrow = "Disc Harrow";
+      String powerTiller = "Power Tiller";
+      String seeder = "Seeder";
+      String transplanter = "Transplanter";
+      String irrigationPipe = "Irrigation Pipe";
+      String dripIrrigationKit = "Drip Irrigation Kit";
+      String fertilizerSpreader = "Fertilizer Spreader";
+      String soilTestingKit = "Soil Testing Kit";
+      String miniTractor = "Mini Tractor";
+
+      String farmingTools[] = {
+         tractor,
+         plough,
+         hoe,
+         shovel,
+         spade,
+         sickle,
+         rake,
+         cultivator,
+         seedDrill,
+         harvester,
+         rotavator,
+         sprayer,
+         waterPump,
+         thresher,
+         wheelbarrow,
+         axe,
+         pickaxe,
+         handWeeder,
+         pruningShears,
+         gardenFork,
+         leveler,
+         discHarrow,
+         powerTiller,
+         seeder,
+         transplanter,
+         irrigationPipe,
+         dripIrrigationKit,
+         fertilizerSpreader,
+         soilTestingKit,
+         miniTractor
+      };
+
+    
+
+      for(String farmingTool : farmingTools) {
+         System.out.println(farmingTool);
+      }
+
+   }
+
+}
