a1e16e6e905d05d76eed258f23c28671cb6ef408
priyamnaik9066-eng <priyamnaik9066@gmail.com>
2026-07-15 10:06:22 +0530

    java code

---
+++ VTUcolleges.java
@@ -0,0 +1,81 @@
+class VTUColleges {
+
+    public static void main(String[] args) {
+
+        String ubdt = "University B.D.T. College of Engineering, Davanagere";
+        String gecr = "Government Engineering College, Ramanagaram";
+        String gech = "Government Engineering College, Hassan";
+        String gecm = "Government Engineering College, Mandya";
+        String gecc = "Government Engineering College, Chamarajanagar";
+        String geck = "Government Engineering College, Kushalnagar";
+        String gechv = "Government Engineering College, Haveri";
+        String geckr = "Government Engineering College, Karwar";
+        String geckrp = "Government Engineering College, K.R. Pet";
+        String geckp = "Government Engineering College, Koppal";
+
+        String sksjit = "Government S.K.S.J.T. Institute of Technology, Bangalore";
+        String rvce = "R.V. College of Engineering (RVCE), Bangalore";
+        String bmsce = "B.M.S. College of Engineering (BMSCE), Bangalore";
+        String rit = "Ramaiah Institute of Technology (RIT), Bangalore";
+        String dsce = "Dayananda Sagar College of Engineering (DSCE), Bangalore";
+        String bit = "Bangalore Institute of Technology (BIT), Bangalore";
+        String bmsit = "B.M.S. Institute of Technology and Management (BMSIT), Bangalore";
+        String sirmvit = "Sir M. Visvesvaraya Institute of Technology (Sir MVIT), Bangalore";
+        String nhce = "New Horizon College of Engineering (NHCE), Bangalore";
+        String nmit = "Nitte Meenakshi Institute of Technology (NMIT), Bangalore";
+
+        String rnsit = "R.N.S. Institute of Technology (RNSIT), Bangalore";
+        String cmrit = "C.M.R. Institute of Technology (CMRIT), Bangalore";
+        String mvjce = "M.V.J. College of Engineering (MVJCE), Bangalore";
+        String drait = "Dr. Ambedkar Institute of Technology (Dr. AIT), Bangalore";
+        String oxford = "The Oxford College of Engineering, Bangalore";
+        String jssate = "J.S.S. Academy of Technical Education (JSSATE), Bangalore";
+        String bnmit = "B.N.M. Institute of Technology (BNMIT), Bangalore";
+        String gat = "Global Academy of Technology (GAT), Bangalore";
+        String sjbit = "S.J.B. Institute of Technology (SJBIT), Bangalore";
+        String ait = "Acharya Institute of Technology (AIT), Bangalore";
+
+        String cambridge = "Cambridge Institute of Technology, Bangalore";
+        String dbit = "Don Bosco Institute of Technology (DBIT), Bangalore";
+        String vemana = "Vemana Institute of Technology, Bangalore";
+        String atria = "Atria Institute of Technology, Bangalore";
+        String saptagiri = "Saptagiri College of Engineering, Bangalore";
+        String hkbk = "H.K.B.K. College of Engineering, Bangalore";
+        String eastwest = "East West Institute of Technology, Bangalore";
+        String ksit = "Kammavari Sangham Institute of Technology (KSIT), Bangalore";
+        String dsatm = "Dayananda Sagar Academy of Technology and Management (DSATM), Bangalore";
+        String alliance = "Alliance College of Engineering and Design, Bangalore";
+
+        String aps = "A.P.S. College of Engineering, Bangalore";
+        String eastpoint = "East Point College of Engineering and Technology, Bangalore";
+        String impact = "Impact College of Engineering, Bangalore";
+        String reva = "Reva Institute of Technology and Management, Bangalore";
+        String rgit = "Rajiv Gandhi Institute of Technology, Bangalore";
+        String rrit = "R.R. Institute of Technology, Bangalore";
+        String srsit = "Sri Revana Siddeshwara Institute of Technology, Bangalore";
+        String vivekananda = "Vivekananda Institute of Technology, Bangalore";
+        String btl = "B.T.L. Institute of Technology and Management, Bangalore";
+
+        String nie = "The National Institute of Engineering (NIE), Mysuru";
+        String sjce = "Sri Jayachamarajendra College of Engineering (SJCE), Mysuru";
+        String vvce = "Vidyavardhaka College of Engineering (VVCE), Mysuru";
+        String nieit = "NIE Institute of Technology, Mysuru";
+        String pesce = "P.E.S. College of Engineering (PESCE), Mandya";
+        String mce = "Malnad College of Engineering (MCE), Hassan";
+        String ritHassan = "Rajeev Institute of Technology, Hassan";
+        String aitChikmagalur = "Adichunchanagiri Institute of Technology, Chikmagalur";
+
+        String[] vtuColleges = {
+            ubdt, gecr, gech, gecm, gecc, geck, gechv, geckr, geckrp, geckp,
+            sksjit, rvce, bmsce, rit, dsce, bit, bmsit, sirmvit, nhce, nmit,
+            rnsit, cmrit, mvjce, drait, oxford, jssate, bnmit, gat, sjbit, ait,
+            cambridge, dbit, vemana, atria, saptagiri, hkbk, eastwest, ksit, dsatm, alliance,
+            aps, eastpoint, impact, reva, rgit, rrit, srsit, vivekananda, btl,
+            nie, sjce, vvce, nieit, pesce, mce, ritHassan, aitChikmagalur
+        };
+
+        for(String college : vtuColleges) {
+            System.out.println(college);
+        }
+    }
+}
