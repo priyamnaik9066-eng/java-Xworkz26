18d800c1a383408ca40d3cad31899f0bafe957a3
priyamnaik9066-eng <priyamnaik9066@gmail.com>
2026-07-15 09:55:57 +0530

    java code

---
+++ SBI.java
@@ -0,0 +1,93 @@
+ class SBI {
+
+    public static void main(String[] args) {
+
+        String sbiMGRoad = "State Bank of India, M.G. Road, Bengaluru";
+        String sbiBrigadeRoad = "State Bank of India, Brigade Road, Bengaluru";
+        String sbiIndiranagar = "State Bank of India, Indiranagar, Bengaluru";
+        String sbiJayanagar = "State Bank of India, Jayanagar, Bengaluru";
+        String sbiJPNagar = "State Bank of India, J.P. Nagar, Bengaluru";
+        String sbiBanashankari = "State Bank of India, Banashankari, Bengaluru";
+        String sbiBasavanagudi = "State Bank of India, Basavanagudi, Bengaluru";
+        String sbiRajajinagar = "State Bank of India, Rajajinagar, Bengaluru";
+        String sbiMalleshwaram = "State Bank of India, Malleshwaram, Bengaluru";
+        String sbiYeshwanthpur = "State Bank of India, Yeshwanthpur, Bengaluru";
+        String sbiHebbal = "State Bank of India, Hebbal, Bengaluru";
+        String sbiRTNagar = "State Bank of India, R.T. Nagar, Bengaluru";
+        String sbiKoramangala = "State Bank of India, Koramangala, Bengaluru";
+        String sbiHSRLayout = "State Bank of India, HSR Layout, Bengaluru";
+        String sbiBTMLayout = "State Bank of India, BTM Layout, Bengaluru";
+        String sbiElectronicCity = "State Bank of India, Electronic City, Bengaluru";
+        String sbiWhitefield = "State Bank of India, Whitefield, Bengaluru";
+        String sbiMarathahalli = "State Bank of India, Marathahalli, Bengaluru";
+        String sbiBellandur = "State Bank of India, Bellandur, Bengaluru";
+        String sbiSarjapurRoad = "State Bank of India, Sarjapur Road, Bengaluru";
+        String sbiKRPuram = "State Bank of India, K.R. Puram, Bengaluru";
+        String sbiMahadevapura = "State Bank of India, Mahadevapura, Bengaluru";
+        String sbiKRMarket = "State Bank of India, K.R. Market, Bengaluru";
+        String sbiMajestic = "State Bank of India, Majestic, Bengaluru";
+        String sbiChickpet = "State Bank of India, Chickpet, Bengaluru";
+        String sbiShivajinagar = "State Bank of India, Shivajinagar, Bengaluru";
+        String sbiFrazerTown = "State Bank of India, Frazer Town, Bengaluru";
+        String sbiUlsoor = "State Bank of India, Ulsoor, Bengaluru";
+        String sbiDomlur = "State Bank of India, Domlur, Bengaluru";
+        String sbiHennur = "State Bank of India, Hennur, Bengaluru";
+        String sbiKengeri = "State Bank of India, Kengeri, Bengaluru";
+        String sbiNagarbhavi = "State Bank of India, Nagarbhavi, Bengaluru";
+        String sbiVijayanagar = "State Bank of India, Vijayanagar, Bengaluru";
+        String sbiRRNagar = "State Bank of India, Rajarajeshwari Nagar, Bengaluru";
+        String sbiMysoreRoad = "State Bank of India, Mysore Road, Bengaluru";
+        String sbiPeenya = "State Bank of India, Peenya, Bengaluru";
+        String sbiDasarahalli = "State Bank of India, Dasarahalli, Bengaluru";
+        String sbiNagasandra = "State Bank of India, Nagasandra, Bengaluru";
+        String sbiYelahanka = "State Bank of India, Yelahanka, Bengaluru";
+        String sbiDevanahalli = "State Bank of India, Devanahalli, Bengaluru";
+
+        String[] sbiBranches = {
+            sbiMGRoad,
+            sbiBrigadeRoad,
+            sbiIndiranagar,
+            sbiJayanagar,
+            sbiJPNagar,
+            sbiBanashankari,
+            sbiBasavanagudi,
+            sbiRajajinagar,
+            sbiMalleshwaram,
+            sbiYeshwanthpur,
+            sbiHebbal,
+            sbiRTNagar,
+            sbiKoramangala,
+            sbiHSRLayout,
+            sbiBTMLayout,
+            sbiElectronicCity,
+            sbiWhitefield,
+            sbiMarathahalli,
+            sbiBellandur,
+            sbiSarjapurRoad,
+            sbiKRPuram,
+            sbiMahadevapura,
+            sbiKRMarket,
+            sbiMajestic,
+            sbiChickpet,
+            sbiShivajinagar,
+            sbiFrazerTown,
+            sbiUlsoor,
+            sbiDomlur,
+            sbiHennur,
+            sbiKengeri,
+            sbiNagarbhavi,
+            sbiVijayanagar,
+            sbiRRNagar,
+            sbiMysoreRoad,
+            sbiPeenya,
+            sbiDasarahalli,
+            sbiNagasandra,
+            sbiYelahanka,
+            sbiDevanahalli
+        };
+
+        for (String branch : sbiBranches) {
+            System.out.println(branch);
+        }
+    }
+}
