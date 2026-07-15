a674faef0af780865464c84c2cd67449f1bd56a0
priyamnaik9066-eng <priyamnaik9066@gmail.com>
2026-07-15 09:59:14 +0530

    java code

---
+++ BangaloreParks.java
@@ -0,0 +1,65 @@
+class BangaloreParks {
+
+   public static void main(String[] args) {
+
+      String lalbagh = "Lalbagh Botanical Garden";
+      String cubbonPark = "Cubbon Park";
+      String bannerghattaPark = "Bannerghatta Biological Park";
+      String jpPark = "JP Park";
+      String bugleRockPark = "Bugle Rock Park";
+      String ulsoorLakePark = "Ulsoor Lake Park";
+      String freedomPark = "Freedom Park";
+      String sankeyTank = "Sankey Tank";
+      String madiwalaLakePark = "Madiwala Lake Park";
+      String cariappaMemorialPark = "Cariappa Memorial Park";
+      String mnKrishnaRaoPark = "M.N. Krishna Rao Park";
+      String richardsPark = "Richards Park";
+      String jayamahalPark = "Jayamahal Park";
+      String mahatmaGandhiPark = "Mahatma Gandhi Park";
+      String nationalMilitaryMemorialPark = "National Military Memorial Park";
+      String indiraGandhiMusicalFountainPark = "Indira Gandhi Musical Fountain Park";
+      String childrenPark = "Children Park";
+      String agaraLakePark = "Agara Lake Park";
+      String hebbalLakePark = "Hebbal Lake Park";
+      String puttenahalliLakePark = "Puttenahalli Lake Park";
+      String kaikondrahalliLakePark = "Kaikondrahalli Lake Park";
+      String turahalliForestPark = "Turahalli Forest Park";
+      String avalahalliForestPark = "Avalahalli Forest Park";
+      String dhanvantariVana = "Dhanvantari Vana";
+      String vishwaGuruBasavannaBiodiversityPark = "Vishwa Guru Basavanna Biodiversity Park";
+
+      String bangaloreParks[] = {
+         lalbagh,
+         cubbonPark,
+         bannerghattaPark,
+         jpPark,
+         bugleRockPark,
+         ulsoorLakePark,
+         freedomPark,
+         sankeyTank,
+         madiwalaLakePark,
+         cariappaMemorialPark,
+         mnKrishnaRaoPark,
+         richardsPark,
+         jayamahalPark,
+         mahatmaGandhiPark,
+         nationalMilitaryMemorialPark,
+         indiraGandhiMusicalFountainPark,
+         childrenPark,
+         agaraLakePark,
+         hebbalLakePark,
+         puttenahalliLakePark,
+         kaikondrahalliLakePark,
+         turahalliForestPark,
+         avalahalliForestPark,
+         dhanvantariVana,
+         vishwaGuruBasavannaBiodiversityPark
+      };
+
+      for(String park : bangaloreParks) {
+         System.out.println(park);
+      }
+
+   }
+
+}
